01rfcberlin/robocup-blockly

commit 492b8d90c90d86158d5d687e14122d2147aff464

Reset will only remove the current robot position, not the code

src/tasks/WalkAndTurnAndRepeatUntil.js
@@ -28,13 +28,19 @@ const Walk = ({task_properties}) => {
     const [currentNumberBlocks, setCurrentNumberBlocks] = useState(0);
 
     // Initialize the robot position on the field for the given task
-    const reset = () => {
+    const init = () => {
         blockly.setInterpreterIsActive(false);
         blockly.simpleWorkspace.current.workspace.clear();
         let parentBlock = blockly.simpleWorkspace.current.workspace.newBlock('start_block');
         parentBlock.initSvg();
         parentBlock.render();
         parentBlock.moveBy(20,20)
+        reset();
+    };
+
+    // Resets the robot position on the field to the original position
+    const reset = () => {
+        blockly.setInterpreterIsActive(false);
         dispatch(RobotActions.reset());
         dispatch(RobotActions.addRobot(
           task_properties.own_robot.position.x,
@@ -42,17 +48,11 @@ const Walk = ({task_properties}) => {
           task_properties.own_robot.position.rotation * 2*Math.PI/360,
             "left"
         ));
-        dispatch(RobotActions.addRobot(
-            task_properties.opponent_robot.position.x,
-            task_properties.opponent_robot.position.y,
-            task_properties.opponent_robot.position.rotation * 2*Math.PI/360,
-            "right"
-        ));
         dispatch(BallActions.setPosition(task_properties.ball.position.x,task_properties.ball.position.y));
     };
 
     useEffect(() => {
-        reset();
+        init();
     }, [task_properties]);
 
     return(

src/tasks/Kick.js
@@ -28,13 +28,19 @@ const Kick = ({task_properties}) => {
     const [currentNumberBlocks,setCurrentNumberBlocks] = useState(0);
 
     // Initialize the robot position on the field for the given task
-    const reset = () => {
+    const init = () => {
         blockly.setInterpreterIsActive(false);
         blockly.simpleWorkspace.current.workspace.clear();
         let parentBlock = blockly.simpleWorkspace.current.workspace.newBlock('start_block');
         parentBlock.initSvg();
         parentBlock.render();
         parentBlock.moveBy(20,20)
+        reset();
+    };
+
+    // Resets the robot position on the field to the original position
+    const reset = () => {
+        blockly.setInterpreterIsActive(false);
         dispatch(RobotActions.reset());
         dispatch(RobotActions.addRobot(
           task_properties.own_robot.position.x,
@@ -46,7 +52,7 @@ const Kick = ({task_properties}) => {
     };
 
     useEffect(() => {
-        reset();
+        init();
     }, [task_properties]);
 
     return(

src/tasks/Walk.js
@@ -28,13 +28,19 @@ const Walk = ({task_properties}) => {
     const [currentNumberBlocks, setCurrentNumberBlocks] = useState(0);
 
     // Initialize the robot position on the field for the given task
-    const reset = () => {
+    const init = () => {
         blockly.setInterpreterIsActive(false);
         blockly.simpleWorkspace.current.workspace.clear();
         let parentBlock = blockly.simpleWorkspace.current.workspace.newBlock('start_block');
         parentBlock.initSvg();
         parentBlock.render();
         parentBlock.moveBy(20,20)
+        reset();
+    };
+
+    // Resets the robot position on the field to the original position
+    const reset = () => {
+        blockly.setInterpreterIsActive(false);
         dispatch(RobotActions.reset());
         dispatch(RobotActions.addRobot(
           task_properties.own_robot.position.x,
@@ -46,15 +52,8 @@ const Walk = ({task_properties}) => {
     };
 
     useEffect(() => {
-        reset();
+        init();
     }, [task_properties]);
-
-    useEffect(() => {
-        let parentBlock = blockly.simpleWorkspace.current.workspace.newBlock('start_block');
-        parentBlock.initSvg();
-        parentBlock.render();
-        parentBlock.moveBy(20,20)
-    }, []);
 
     return(
         <div>

src/tasks/WalkAndRepeat.js
@@ -29,13 +29,19 @@ const WalkAndRepeat = ({task_properties}) => {
 
 
     // Initialize the robot position on the field for the given task
-    const reset = () => {
+    const init = () => {
         blockly.setInterpreterIsActive(false);
         blockly.simpleWorkspace.current.workspace.clear();
         let parentBlock = blockly.simpleWorkspace.current.workspace.newBlock('start_block');
         parentBlock.initSvg();
         parentBlock.render();
         parentBlock.moveBy(20,20)
+        reset();
+    };
+
+    // Resets the robot position on the field to the original position
+    const reset = () => {
+        blockly.setInterpreterIsActive(false);
         dispatch(RobotActions.reset());
         dispatch(RobotActions.addRobot(
           task_properties.own_robot.position.x,
@@ -47,7 +53,7 @@ const WalkAndRepeat = ({task_properties}) => {
     };
 
     useEffect(() => {
-        reset();
+        init();
     }, [task_properties]);
 
     return(

src/tasks/WalkAndRepeatUntil.js
@@ -29,13 +29,19 @@ const WalkAndRepeatUntil = ({task_properties}) => {
 
 
     // Initialize the robot position on the field for the given task
-    const reset = () => {
+    const init = () => {
         blockly.setInterpreterIsActive(false);
         blockly.simpleWorkspace.current.workspace.clear();
         let parentBlock = blockly.simpleWorkspace.current.workspace.newBlock('start_block');
         parentBlock.initSvg();
         parentBlock.render();
         parentBlock.moveBy(20,20)
+        reset();
+    };
+
+    // Resets the robot position on the field to the original position
+    const reset = () => {
+        blockly.setInterpreterIsActive(false);
         dispatch(RobotActions.reset());
         dispatch(RobotActions.addRobot(
           task_properties.own_robot.position.x,
@@ -47,7 +53,7 @@ const WalkAndRepeatUntil = ({task_properties}) => {
     };
 
     useEffect(() => {
-        reset();
+        init();
     }, [task_properties]);
 
     return(

src/tasks/WalkAndTurn.js
@@ -29,13 +29,19 @@ const WalkAndTurn = ({task_properties}) => {
 
 
     // Initialize the robot position on the field for the given task
-    const reset = () => {
+    const init = () => {
         blockly.setInterpreterIsActive(false);
         blockly.simpleWorkspace.current.workspace.clear();
         let parentBlock = blockly.simpleWorkspace.current.workspace.newBlock('start_block');
         parentBlock.initSvg();
         parentBlock.render();
         parentBlock.moveBy(20,20)
+        reset();
+    };
+
+    // Resets the robot position on the field to the original position
+    const reset = () => {
+        blockly.setInterpreterIsActive(false);
         dispatch(RobotActions.reset());
         dispatch(RobotActions.addRobot(
           task_properties.own_robot.position.x,
@@ -47,7 +53,7 @@ const WalkAndTurn = ({task_properties}) => {
     };
 
     useEffect(() => {
-        reset();
+        init();
     }, [task_properties]);
 
 

commit 22e2d212641b1d3b14ed9f93ffb9d928434d9b78

[FIX] Instantiate missing opponent robot in level 8
My commit 492b8d90 accidentally deleted the opponent robot in level 8.
This commit adds it back in.

src/tasks/WalkAndTurnAndRepeatUntil.js
@@ -48,6 +48,12 @@ const Walk = ({task_properties}) => {
           task_properties.own_robot.position.rotation * 2*Math.PI/360,
             "left"
         ));
+        dispatch(RobotActions.addRobot(
+            task_properties.opponent_robot.position.x,
+            task_properties.opponent_robot.position.y,
+            task_properties.opponent_robot.position.rotation * 2*Math.PI/360,
+            "right"
+        ));
         dispatch(BallActions.setPosition(task_properties.ball.position.x,task_properties.ball.position.y));
     };
 