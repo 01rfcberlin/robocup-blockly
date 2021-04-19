eee146279c7efbc94800ad6a8dc8801058248a3f
Maike Paetzel <maike.paetzel@it.uu.se>
2021-04-19 22:25:19 +0200

    [Fix] Fixed bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import reduxStore from "./reduxStore";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <React.StrictMode>
