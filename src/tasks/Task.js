59a918ca5ba23769faf459e60a0b3c6dbe283cc0
André Schröder <andre.schroedr@gmail.com>
2021-04-26 00:20:56 +0200

    [patch] add link to privacy policy

--- src/tasks/Task.js
+++ src/tasks/Task.js
@@ -298,6 +298,7 @@ export default function Task(props) {
                               <img alt="Click this for hints" height={"100px"} src={process.env.PUBLIC_URL + "/bueroklammer.jpg"} onClick={() => {toggleTip()}}></img>
                           </Col>
                       </Row>
+                      <Row><a href="https://01.rfc-berlin.de/privacy-policy/">Datenschutzerklärung</a></Row>
                   </Col>
                   <Col xs={1}/>
                   <Col xs={7}>
