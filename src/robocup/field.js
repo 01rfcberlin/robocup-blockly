1852ff03a275738f5d646db5bbe736217c0419f2
André Schröder <andre.schroedr@gmail.com>
2022-03-26 12:13:20 +0100

    [feat] add tada animation

--- src/robocup/field.js
+++ src/robocup/field.js
@@ -9,6 +9,7 @@ import * as canvas from "./canvas";
 import * as images from "./images";
 import Alert from "react-bootstrap/Alert";
 import InterfaceActions from "./InterfaceActions";
+import Particles from "react-tsparticles";
 
 /**
  * Handles drawing the background of the field as well as the robot(s)
@@ -287,11 +288,151 @@ export const RoboCupField = ({grid_properties}) => {
 
     return (
         <div>
-            {toggleGoalAlert && !ball.isMoving &&
+            {toggleGoalAlert && !ball.isMoving && <React.Fragment>
+            <Particles id="tsparticles"
+
+          width={constants.canvas.width}
+          height={constants.canvas.height}
+
+        style={{
+          position: "absolute",
+            left: 0,
+            top: 0,
+        }}
+
+      options={{
+
+  fullScreen: { enable: false },
+
+  "particles": {
+    "number": {
+      "value": 0
+    },
+    "color": {
+      "value": [
+        "#00FFFC",
+        "#FC00FF",
+        "#fffc00"
+      ]
+    },
+    "shape": {
+      "type": "circle",
+      "options": {}
+    },
+    "opacity": {
+      "value": 1,
+      "animation": {
+        "enable": true,
+        "minimumValue": 0,
+        "speed": 2,
+        "startValue": "max",
+        "destroy": "min"
+      }
+    },
+    "size": {
+      "value": 4,
+      "random": {
+        "enable": true,
+        "minimumValue": 2
+      }
+    },
+    "links": {
+      "enable": false
+    },
+    "life": {
+      "duration": {
+        "sync": true,
+        "value": 5
+      },
+      "count": 1
+    },
+    "move": {
+      "enable": true,
+      "gravity": {
+        "enable": true,
+        "acceleration": 10
+      },
+      "speed": {
+        "min": 10,
+        "max": 20
+      },
+      "decay": 0.1,
+      "direction": "none",
+      "straight": false,
+      "outModes": {
+        "default": "destroy",
+        "top": "none"
+      }
+    },
+    "rotate": {
+      "value": {
+        "min": 0,
+        "max": 360
+      },
+      "direction": "random",
+      "move": true,
+      "animation": {
+        "enable": true,
+        "speed": 60
+      }
+    },
+    "tilt": {
+      "direction": "random",
+      "enable": true,
+      "move": true,
+      "value": {
+        "min": 0,
+        "max": 360
+      },
+      "animation": {
+        "enable": true,
+        "speed": 60
+      }
+    },
+    "roll": {
+      "darken": {
+        "enable": true,
+        "value": 25
+      },
+      "enable": true,
+      "speed": {
+        "min": 15,
+        "max": 25
+      }
+    },
+    "wobble": {
+      "distance": 30,
+      "enable": true,
+      "move": true,
+      "speed": {
+        "min": -15,
+        "max": 15
+      }
+    }
+  },
+  "emitters": {
+    "life": {
+      "count": 0,
+      "duration": 0.1,
+      "delay": 0.4
+    },
+    "rate": {
+      "delay": 0.1,
+      "quantity": 150
+    },
+    "size": {
+      "width": 0,
+      "height": 0
+    }
+  },
+
+  }}
+              />
             <Alert variant={'success'} style={{position: "absolute", zIndex:10}} onClose={() => dispatch(InterfaceActions.toggleGoalAlert(false))} dismissible>
                 <Alert.Heading>Toooooor!</Alert.Heading>
                 <p>Sehr gut, du hast die Aufgabe gelöst. Jetzt kannst du weiter mit der nächsten Aufgabe machen.</p>
             </Alert>
+</React.Fragment>
             }
             {toggleOwnGoalAlert && !ball.isMoving &&
             <Alert variant={'warning'} style={{position: "absolute", zIndex:10}} onClose={() => dispatch(InterfaceActions.toggleOwnGoalAlert(false))} dismissible>
